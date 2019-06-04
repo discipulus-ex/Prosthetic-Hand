c9717d3b2e18049e5f9c37a9e1e072768bb335de
DDuunk <dylanduunk@live.nl>
2019-06-04 21:02:05 +0200

    Removed unused code

--- src/controllerHand/main.cpp
+++ src/controllerHand/main.cpp
@@ -2,15 +2,16 @@
 
 void setup() {
     // Start serial monitor
-    Serial.begin(BAUD_RATE);
+    Serial.begin(9600);
 
     // Attach pins to the servo's
-    for (int i = 0; i < sizeof(handServos); i++) {
+    for (int i = 0; i < sizeof(handServos) / sizeof(handServos[0]); i++) {
+        Serial.println(i);
         handServos[i].attach(i + beginServoPin);
     }
 
     // Configure pinMode for each button
-    for (int i = 0; i < sizeof(fingerButtons); i++) {
+    for (int i = 0; i < sizeof(fingerButtons) / sizeof(fingerButtons[0]); i++) {
         pinMode(fingerButtons[i].pin, INPUT_PULLUP);
     }
 }
@@ -22,7 +23,7 @@ void loop() {
     val = map(val, 0, 1023, 0, 180);
 
     // Iterate over the fingers
-    for (int i = 0; i < sizeof(fingerButtons); i++) {
+    for (int i = 0; i < sizeof(fingerButtons) / sizeof(fingerButtons[0]); i++) {
         fingerButtons[i].reading = digitalRead(fingerButtons[i].pin);
 
         // Makes the each button act like a switch
@@ -35,19 +36,20 @@ void loop() {
             time = millis();        
         }
 
-        if (fingerButtons[i].state) {
+        if (fingerButtons[i].state == HIGH) {
             // Makes the fingers response to the potmeter when
             // button state == HIGH
             handServos[i].write(val);
 
             // Print te value of potmeter with is asociated finger
-            Serial.print(fingerButtons[i].name);
-            Serial.print(" ");
-            Serial.println(val);
+            // Serial.print(fingerButtons[i].name);
+            // Serial.print(" ");
+            // Serial.println(val);
 
             delay(15);
         } else {
             handServos[i].write(0);
+            Serial.println("UIT");
             delay(15);
         }
 
--- src/controllerHand/main.h
+++ src/controllerHand/main.h
@@ -1,11 +1,8 @@
 #pragma once
  
 #include "Servo.h"
-#include "Keypad.h"
 
-#define BAUD_RATE 9600
-
-Servo handServos[4]; // A servo for each finger      
+Servo handServos[5]; // A servo for each finger      
 int beginServoPin = 8; // Start counting from pin 8 for the servo's
 
 struct Button {
@@ -18,14 +15,14 @@ struct Button {
 
 // Pre-defined buttons for each finger
 Button fingerButtons[] = {
-    { "Thumb", 5, HIGH, 0, LOW }, //TODO: Change pin
-    { "Indexfinger", 6, HIGH, 0, LOW }, //TODO: Change pin
-    { "Middlefinger", 2, HIGH, 0, LOW },
-    { "Ringfinger", 3, HIGH, 0, LOW },
-    { "Pink", 4, HIGH, 0, LOW },
-}
+    { "Thumb", 2, HIGH, 0, LOW },
+    { "Indexfinger", 3, HIGH, 0, LOW },
+    { "Middlefinger", 4, HIGH, 0, LOW },
+    { "Ringfinger", 5, HIGH, 0, LOW },
+    { "Pinky", 6, HIGH, 0, LOW },
+};
 
-int potPin = 0;
+int potPin = A0;
 int val;
 
 long time = 0;
